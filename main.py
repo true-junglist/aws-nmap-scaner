11e91df8d68b096cf8115066bd803d6591f31cc3
true-junglist <14940286+true-junglist@users.noreply.github.com>
2021-08-14 19:32:44 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,40 @@
-# This is a sample Python script.
+import boto3
+import boto3.utils
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+client = boto3.client('ec2', region_name='eu-west-1')
+# client = boto3.client('ec2')
+
+response = client.describe_instances(
+    MaxResults=20,
+)
+
+for r in response['Reservations']:
+    for i in r['Instances']:
+        tag_values_list = []
+        for tags in i['Tags']:
+            for key, value in tags.items():
+                tag_values_list.append(value)
+                print('id:', i['InstanceId'], 'zone:', i['Placement']['AvailabilityZone'], 'state:', i['State']['Name'],
+                      i['PublicIpAddress'],
+                      'tags:',
+                      tag_values_list)
+
+# def create_conn(aws_access_key, aws_secret_key, ec2_region):
+#     conn = boto3.client('ec2', aws_access_key_id=aws_access_key, aws_secret_access_key=aws_secret_key, region_name=ec2_region)
+#     return conn
+
+# def handler(context, inputs):
+#     ec2 = boto3.resource('ec2')
+#     filters = [{
+#         'Name': 'instance-state-name',
+#         'Values': ['running']
+#     }]
+#
+#     instances = ec2.instances.filter(Filters=filters)
+#     for instance in instances:
+#         print('Instance: ' + instance.id)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# ec2 = boto3.client('ec2')
+# response = ec2.describe_instances()
+# print(response)
