d7792cb00464e4957c4bfdd135b9f915e0fbbcd2
true-junglist <mamba.aguzarova@gmail.com>
2021-08-16 11:59:48 +0300

    test

--- run_id.py
+++ run_id.py
@@ -1,8 +1,8 @@
 import boto3
 from botocore.exceptions import ClientError
 
-instance_id = 'i-06edd4f2a87e541a8'
-action = ''
+id = 'i-06edd4f2a87e541a8'
+action = 'ON'
 region = 'eu-west-1'
 
 ec2 = boto3.client('ec2', region)
@@ -12,37 +12,37 @@ ec2 = boto3.client('ec2', region)
 if action == 'ON':
     # Do a dryrun first to verify permissions
     try:
-        ec2.start_instances(InstanceIds=[instance_id], DryRun=True)
+        ec2.start_instances(InstanceIds=[id], DryRun=True)
     except ClientError as e:
         if 'DryRunOperation' not in str(e):
             raise
 
     # Dry run succeeded, run start_instances without dryrun
     try:
-        response = ec2.start_instances(InstanceIds=[instance_id], DryRun=False)
-        print(response)
+        response = ec2.start_instances(InstanceIds=[id], DryRun=False)
+        # print(response)
     except ClientError as e:
         print(e)
 else:
     # Do a dryrun first to verify permissions
     try:
-        ec2.stop_instances(InstanceIds=[instance_id], DryRun=True)
+        ec2.stop_instances(InstanceIds=[id], DryRun=True)
     except ClientError as e:
         if 'DryRunOperation' not in str(e):
             raise
 
     # Dry run succeeded, call stop_instances without dryrun
     try:
-        response = ec2.stop_instances(InstanceIds=[instance_id], DryRun=False)
+        response = ec2.stop_instances(InstanceIds=[id], DryRun=False)
         print(response)
     except ClientError as e:
         print(e)
 
-def get_public_ip(id):
-    ec2_client = boto3.client("ec2", region_name="us-west-2")
-    reservations = ec2_client.describe_instances(InstanceIds=[id]).get("Reservations")
+def get_public_ip(instance_id):
+    ec2_client = boto3.client("ec2", region)
+    reservations = ec2_client.describe_instances(InstanceIds=[instance_id]).get("Reservations")
 
     for reservation in reservations:
         for instance in reservation['Instances']:
             print(instance.get("PublicIpAddress"))
-# get_public_ip(instance_id)
+get_public_ip(id)
